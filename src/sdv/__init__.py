b7967749528f677c673f266aa26952052b177bda
Olga Lyashevska <olga.lyashevska@gmail.com>
2024-09-03 10:37:38 +0200

    init commit

---
+++ src/sdv/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about sdv."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "dfv"
+__email__ = "sdhg@afvs.afs"
+__version__ = "0.1.0"
